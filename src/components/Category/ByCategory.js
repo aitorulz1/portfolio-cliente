89e69c4cbfcad5a0d34725b1d7055a9ed0648832
Aitor Arina <aitorulz1@gmail.com>
2021-02-24 19:46:47 +0100

    Getting elements by category

--- src/components/Category/ByCategory.js
+++ src/components/Category/ByCategory.js
@@ -1,13 +1,37 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import CategoryItems from '../Category/CategoryItems';
 
 import './ByCategory.css';
 
+import clienteAxios from 'axios';
 
 export default function ByCategory() {
-    return (
-        <div>
-    
-        </div>
-    )
+
+    const [ porcategoria, guardarPorCategoria] = useState([])
+
+    useEffect(() => {
+        const proyectosFiltrados = async() => {
+            try {
+                const resultado = await clienteAxios.get('/category/:category')
+                console.log(resultado)
+                guardarPorCategoria(resultado)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        proyectosFiltrados();
+    }, [])
+
+
+            return (
+                <ul>
+                {/* {proyectos.map(proyecto => (
+                    <CategoryItems
+                        key= {proyecto.id}
+                        proyecto= {proyecto}
+                    />
+                ))} */}
+                hola
+            </ul>
+            )
 }
